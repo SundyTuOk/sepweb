c8e2ab54ab461f73b00bf14fcdc3c6fb0e62230f
1129830650@qq.com <1129830650@qq.com>
2017-10-09 17:46:57 +0800

    写了一个spring测试类，但是现在通过c3p0连不上数据库，暂时不知道原因
    Signed-off-by: 1129830650@qq.com <1129830650@qq.com>

--- src/test/java/com/sf/login/TuTest.java
+++ src/test/java/com/sf/login/TuTest.java
@@ -7,7 +7,12 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 import javax.annotation.Resource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @RunWith(SpringJUnit4ClassRunner.class)//表示整合JUnit4进行测试
 @ContextConfiguration(locations={"classpath:config/spring/applicationContext.xml"})//加载spring配置文件
@@ -16,11 +21,21 @@ public class TuTest {
     @Resource(name = "sessionFactory")
     private SessionFactory sessionFactory;
 
-    @Resource(name = "dataSource")
+    @Resource(name = "mdataSource")
     private ComboPooledDataSource dataSource;
 
     @Test
     public void test1(){
-        System.out.println(dataSource.getClass().toString());
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM apsaleinfo where rownum=1");
+            ResultSet resultSet = ps.executeQuery();
+            if(resultSet.next()){
+                String saleinfonum = resultSet.getString("SALEINFONUM");
+                System.out.println(saleinfonum);
+            }
+        } catch (SQLException e) {
+            System.out.println("出错"+e.toString());
+        }
     }
 }
