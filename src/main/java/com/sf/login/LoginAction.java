c0490d68cafb7aa75da31c38096d9a666a557e8e
1129830650@qq.com <1129830650@qq.com>
2017-10-10 17:49:21 +0800

    写了一个spring测试类，但是现在通过c3p0连不上数据库，暂时不知道原因
    Signed-off-by: 1129830650@qq.com <1129830650@qq.com>

--- src/main/java/com/sf/login/LoginAction.java
+++ src/main/java/com/sf/login/LoginAction.java
@@ -3,6 +3,8 @@ package com.sf.login;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.opensymphony.xwork2.ActionContext;
 import org.apache.struts2.util.ServletContextAware;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
@@ -14,16 +16,18 @@ public class LoginAction implements ServletContextAware{
     public String driverClass;
 
     public String login(){
-//        WebApplicationContext context =
-//                (WebApplicationContext) servletContext.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
-//        ComboPooledDataSource bean = (ComboPooledDataSource) context.getBean("dataSource");
+        WebApplicationContext context =
+                (WebApplicationContext) servletContext.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        SessionFactory bean = (SessionFactory) context.getBean("sessionFactory");
+        Session session = bean.openSession();
+//        session.s
 //        driverClass = bean.getDriverClass();
         driverClass = "我就是啦啦啦啦";
         return "success";
     }
 
     public void setServletContext(ServletContext context) {
-
+        this.servletContext = context;
     }
 
     public String getDriverClass() {
